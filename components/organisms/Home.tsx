5ff0083af224e356128ade53bc60f68d1aac01fa
marsidorowicz@gmail.com <marsidorowicz@gmail.com>
2022-12-12 11:51:49 +0100

    fix length error

--- components/organisms/Home.tsx
+++ components/organisms/Home.tsx
@@ -1,33 +1,8 @@
 /** @format */
 
 import * as React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-
-import useTranslation from 'next-translate/useTranslation'
-
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { setTitleAction } from '../../state/action-creators'
-import { UseLocalStorage } from '../../utilities/UseLocalStorage'
 
 const Home = (props: { children: any }) => {
-	const dispatch = useDispatch()
-	const state = useSelector((state: any) => state.root)
-	const { t } = useTranslation('home')
-	const router = useRouter()
-	const [title, setTitle] = UseLocalStorage('title', '')
-
-	if (typeof window !== 'undefined') {
-		let url = new URL(window.location.href)
-		console.log(url.search)
-	}
-
-	useEffect(() => {
-		if (!title) return
-		dispatch(setTitleAction(title))
-		return () => {}
-	}, [])
-
 	return <div>{props?.children}</div>
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,13 @@ import MainPoster from '../components/atoms/MainPoster'
 import Home from '../components/organisms/Home'
 import req from '../utilities/apiReqs'
 import MovieThumbnail from '../components/atoms/MovieThumbnail'
+import { UseLocalStorage } from '../utilities/UseLocalStorage'
+import MovieThumbnailNotRated from '../components/atoms/MovieThumbnailNotRated'
+import useTranslation from 'next-translate/useTranslation'
+import { useRouter } from 'next/router'
+import { useDispatch, useSelector } from 'react-redux'
+import { setTitleAction } from '../state/action-creators'
+import { BrowserRouter, Link, Route, Routes, useNavigate } from 'react-router-dom'
 
 export async function getServerSideProps(context: any) {
 	const [topRated, genre] = await Promise.all([fetch(req.topRated).then((res) => res.json()), fetch(req.genreList).then((res) => res.json()), ,])
@@ -18,29 +25,43 @@ export async function getServerSideProps(context: any) {
 	}
 }
 
-export default function HomePage(props: { topRated: any; genre: any }) {
+export function Root(props: { topRated: any; genre: any }) {
 	const [showChild, setShowChild] = useState(false)
 	const [topRatedArray, setTopRatedArray] = useState<[]>()
+	const [dataFromFilters, setDataFromFilters] = UseLocalStorage('dataFromFilters', '')
+	const [selectedFilters, setSelectedFilters] = UseLocalStorage('selectedFilters', null)
+	const [year, setYear] = UseLocalStorage('year', '')
+	const [title, setTitle] = UseLocalStorage('title', null)
+	const navigate = useNavigate()
+	const dispatch = useDispatch()
+	const state = useSelector((state: any) => state.root)
+	const { t } = useTranslation('home')
 
-	const getYear = async (props: { year: string; page?: number }) => {
-		if (!props?.year) return
-		const res = await fetch(
-			req.year + `&sort_by=vote_average.desc&sort_by=popularity.desc&primary_release_year=${props?.year}&page=${props?.page ? props?.page.toString() : '1'}`
-		).then((res) => res.json())
-		console.log('res')
-		console.log(res)
+	console.log(dataFromFilters)
+	console.log(topRatedArray)
+
+	if (typeof window !== 'undefined') {
+		let url = new URL(window.location.href)
+
+		console.log(url)
+		console.log(url.search)
+		let arrOfObjects = {}
+		const searchParams: any = new URLSearchParams(url.search)
+		for (const [key, value] of searchParams.entries()) {
+			console.log(`${key}, ${value}`)
+			arrOfObjects[key] = value
+		}
+		console.log('arrOfObjects')
+		console.log(arrOfObjects)
 	}
 
 	useEffect(() => {
-		console.log('topRated')
-		console.log(props?.topRated)
-		getYear({
-			year: '2020',
-			page: 1,
-		})
-		console.log('genre')
-		console.log(props?.genre)
+		if (!title) return
+		dispatch(setTitleAction(title))
+		return () => {}
+	}, [])
 
+	useEffect(() => {
 		setTopRatedArray(props?.topRated)
 		setShowChild(true)
 	}, [])
@@ -49,35 +70,56 @@ export default function HomePage(props: { topRated: any; genre: any }) {
 		return null
 	}
 	return (
-		<Layout genre={props?.genre}>
-			<Home>
-				<MainPoster data={props?.topRated} />
-				<section id='top-rated' className='flex float-left w-full overflow-x-scroll scroll-smooth'>
-					{topRatedArray?.length
-						? topRatedArray.map((movieInfoData: any, index: number) => {
-								if (index === 0 || index > 7) return
-								return (
-									<div key={movieInfoData?.id}>
-										<MovieThumbnail movieInfo={movieInfoData} rating={index} />
-									</div>
-								)
-						  })
-						: 'No Data'}
-				</section>
-				<div>Last Search</div>
-				{/* <section id='searched' className='flex float-left w-full overflow-x-scroll scroll-smooth'>
-					{topRatedArray?.length
-						? topRatedArray.map((movieInfoData: any, index: number) => {
-								if (index === 0 || index > 7) return
-								return (
-									<div key={movieInfoData?.id}>
-										<MovieThumbnail movieInfo={movieInfoData} rating={index} />
-									</div>
-								)
-						  })
-						: 'No Data'}
-				</section> */}
-			</Home>
-		</Layout>
+		<div>
+			<Layout
+				genre={props?.genre}
+				sendData={(data) => {
+					setDataFromFilters(data)
+				}}>
+				<Home>
+					<MainPoster data={props?.topRated} />
+					<section id='top-rated' className='flex float-left w-full overflow-x-scroll scroll-smooth'>
+						{topRatedArray?.length
+							? topRatedArray.map((movieInfoData: any, index: number) => {
+									if (index === 0 || index > 7) return
+									return (
+										<div key={movieInfoData?.id}>
+											<MovieThumbnail movieInfo={movieInfoData} rating={index} />
+										</div>
+									)
+							  })
+							: 'No Data'}
+					</section>
+
+					{year && dataFromFilters && dataFromFilters?.results?.length ? (
+						<div>
+							<div>Last Search By Year</div>
+							<section id='searched-year' className='flex float-left w-full overflow-x-scroll scroll-smooth mb-10'>
+								{dataFromFilters?.results?.length
+									? dataFromFilters?.results?.map((movieInfoData: any, index: number) => {
+											return (
+												<div key={movieInfoData?.id}>
+													<MovieThumbnailNotRated movieInfo={movieInfoData} />
+												</div>
+											)
+									  })
+									: 'No Data'}
+							</section>
+						</div>
+					) : (
+						''
+					)}
+					<Link to={'/play'}>Home</Link>
+				</Home>
+			</Layout>
+			<Routes>
+				<Route path='/' element={<></>} />
+				<Route path='/play' element={<>A</>} />
+			</Routes>
+		</div>
 	)
+}
+
+export default function App(props: { topRated: any; genre: any }) {
+	return <BrowserRouter children={<Root topRated={props?.topRated} genre={props?.genre} />}></BrowserRouter>
 }
