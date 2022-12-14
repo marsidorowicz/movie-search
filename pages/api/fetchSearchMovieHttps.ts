84c34c241c1a753b3c64a795e8ced87192c4a25a
marsidorowicz@gmail.com <marsidorowicz@gmail.com>
2022-12-14 23:42:47 +0100

    test with fetch

--- pages/api/fetchSearchMovieHttps.ts
+++ pages/api/fetchSearchMovieHttps.ts
@@ -2,16 +2,11 @@
 
 import axios, { AxiosResponse } from 'axios'
 import { GetServerSideProps, NextApiRequest, NextApiResponse } from 'next'
-import dotenv from 'dotenv'
-dotenv.config()
+import { API_KEY, BASE_URL_TMDB } from '../../utilities/apiReqs'
 
-const base = process.env.BASE_URL_TMDB
-
-export const API_KEY = process.env.NEXT_PUBLIC_API_KEY_TMDB
 export const FetchSearchMovie = async (req: NextApiRequest, res: NextApiResponse) => {
 	try {
 		const { year, title, genre } = req.body
-		console.log(req.body)
 
 		let query: string = ''
 
@@ -31,26 +26,20 @@ export const FetchSearchMovie = async (req: NextApiRequest, res: NextApiResponse
 		}
 		if (!title) return
 
-		const response = await axios.get(`${base}/search/movie/?${query}&api_key=${API_KEY}`, {
-			headers: { 'Content-Type': 'application/json' },
-			withCredentials: false,
-		})
-		console.log(response.data)
+		const response: any = await fetch(`${BASE_URL_TMDB}/search/movie/?${query}&api_key=${API_KEY}`).then((response) => response.json())
+		// console.log(response)
 
-		if (response?.status !== 200 && response?.statusText !== 'OK') return
+		if (!response) return
 
-		const resFiltered = response?.data?.results?.filter((result: any) => {
+		const resFiltered = response?.results.filter((result: any) => {
+			console.log(result)
+
 			const genresFromResult = result?.genre_ids
 			return genreIds.some((i: any) => genresFromResult.includes(i))
 		})
 
-		console.log('resFiltered')
-		console.log(resFiltered)
-
-		return res.status(200).json({ data: { results: resFiltered?.length > 0 ? resFiltered : response } })
+		return res.status(200).json({ results: resFiltered?.length > 0 ? resFiltered : response })
 	} catch (error) {
-		console.log(error)
-
 		res.status(301).json({ data: JSON.stringify(error) })
 	}
 
